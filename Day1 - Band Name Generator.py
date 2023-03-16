4901244a90854f1862805907065db97d8a1dc6ad
Dalton Jens <126439073+DaltonJens@users.noreply.github.com>
2023-03-16 18:13:18 -0500

    Day 1 Project upload

---
+++ Day1 - Band Name Generator.py
@@ -0,0 +1,12 @@
+print("Welcome to the band name generator")
+
+City = input("City name where you grew up:\n ")  # User inpout data
+Pet = input("Name of your pet:\n ")              # \n is for a new line, like 'enter'
+
+print("")
+print("")
+print("Your band name is the...")    #Output
+print(City + " " + Pet + "'s")
+print("")
+print("")
+print("")
