9a273db87aeb0f997c51f77c902b736dd0316716
Dalton Jens <126439073+DaltonJens@users.noreply.github.com>
2023-04-20 17:08:40 +0800

    Day31(+2)
    Finished project to classes spec, pre-customization

--- Day29 Password Manager/main.py
+++ Day29 Password Manager/main.py
@@ -58,6 +58,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
+
 #Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
