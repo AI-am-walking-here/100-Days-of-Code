3153b189e00fb655c984b7ba158a8868ae425cbc
Dalton Jens <126439073+DaltonJens@users.noreply.github.com>
2023-04-23 16:25:25 +0800

    Day31(+2)
    Basic flash card for Chinese

--- Day31 Flippy Flashy/main.py
+++ Day31 Flippy Flashy/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import pandas
+import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -7,35 +7,40 @@ current_card = {}
 to_learn = {}
 
 try:
-    data = pandas.read_csv("data/words_to_learn.csv")
+    data = pd.read_csv("data/words_to_learn.csv")
 except FileNotFoundError:
-    original_data = pandas.read_csv("data/french_words.csv")
-    print(original_data)
+    original_data = pd.read_csv("data/chinese_vocab.csv")
     to_learn = original_data.to_dict(orient="records")
 else:
     to_learn = data.to_dict(orient="records")
 
 
 def next_card():
-    global current_card, flip_timer
-    window.after_cancel(flip_timer)
+    global current_card
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_title, text="Chinese", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Character"], fill="black")
+    canvas.itemconfig(card_pinyin, text=current_card["Pinyin"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
-    flip_timer = window.after(3000, func=flip_card)
 
 
-def flip_card():
-    canvas.itemconfig(card_title, text="English", fill="white")
-    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
-    canvas.itemconfig(card_background, image=card_back_img)
+def flip_card(event):
+    if canvas.itemcget(card_title, "text") == "Chinese":
+        canvas.itemconfig(card_title, text="English", fill="white")
+        canvas.itemconfig(card_word, text=current_card["Definition"], fill="white")
+        canvas.itemconfig(card_pinyin, text="", fill="white")
+        canvas.itemconfig(card_background, image=card_back_img)
+    else:
+        canvas.itemconfig(card_title, text="Chinese", fill="black")
+        canvas.itemconfig(card_word, text=current_card["Character"], fill="black")
+        canvas.itemconfig(card_pinyin, text=current_card["Pinyin"], fill="black")
+        canvas.itemconfig(card_background, image=card_front_img)
 
 
 def is_known():
     to_learn.remove(current_card)
     print(len(to_learn))
-    data = pandas.DataFrame(to_learn)
+    data = pd.DataFrame(to_learn)
     data.to_csv("data/words_to_learn.csv", index=False)
     next_card()
 
@@ -44,14 +49,13 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer = window.after(3000, func=flip_card)
-
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+card_pinyin = canvas.create_text(400, 350, text="", font=("Arial", 40, "italic"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -65,7 +69,7 @@ known_button.grid(row=1, column=1)
 
 next_card()
 
+# Bind the <Button-1> event to the canvas
+canvas.bind("<Button-1>", flip_card)
+
 window.mainloop()
-
-
-
